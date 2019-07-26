165d09e570ed3f7f81fa2e9c88fb250bf13e8593
Tutbera <giorgi.tutberidze@adjara.com>
2019-07-26 16:39:33 +0400

    first commit

--- src/project/components/player/controls/seeker/seeker.js
+++ src/project/components/player/controls/seeker/seeker.js
@@ -1,25 +1,15 @@
 import React, {Component} from 'react';
 
 class Seeker extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    componentWillUpdate(nextProps, nextState, nextContext) {
-    }
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-
-        console.log(this.props.model, this.props.max);
-        return null;
-    }
 
     render() {
         return (
             <div className='seeker'>
-                {this.props.model } /{ this.props.max}
+                {this.props.model} /{this.props.max}
                 <div
                     className='seeker-inner'
-                style={{'width.%': this.props.model / this.props.max * 100}}
+                    style={{'width': (this.props.model / this.props.max * 100) + '%'}}
                 />
             </div>
         );
--- src/project/components/player/player.js
+++ src/project/components/player/player.js
@@ -18,32 +18,48 @@ let player;
 
 class Player extends Component {
 
+    state = {
+        playerStatus: 'pause',
+        duration: 0,
+        currentTime: 0
+    };
+
     componentDidMount() {
-        socket.on('navigate', (text) => {
+        socket.on('navigate', () => {
             this.props.onPlay(this.props.playerStatus === 'play' ? 'pause' : 'play')
         });
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        this.playserStateHandler();
+     if (this.state.playerStatus !== this.props.playerStatus) {
+         this.playserStateHandler();
+         this.setState({playerStatus : this.props.playerStatus})
+        }
     }
 
+
     render() {
+
         return (
             <React.Fragment>
 
                 <div className='w-100'>
-                    {/*<Button title={this.props.playerStatus === 'play' ? 'pause' : 'play'} clicked={() => {*/}
-                    {/*this.props.onPlay(this.props.playerStatus === 'play' ? 'pause' : 'play')*/}
-                    {/*}}/>*/}
-                    {/*<Seeker max={player.duration} model={player.currentTime }/>*/}
-
-
+                    <Button title={this.props.playerStatus === 'play' ? 'pause' : 'play'} clicked={() => {
+                    this.props.onPlay(this.props.playerStatus === 'play' ? 'pause' : 'play')
+                    }}/>
+                    <Seeker max={this.state.duration} model={this.state.currentTime}/>
                 </div>
                 <div className='w-100 h-100vh d-flex justify-content-center align-items-center '>
 
                     <video
-
+                        onTimeUpdate={
+                            () => {
+                                this.setState({
+                                    duration: player.duration,
+                                    currentTime: player.currentTime
+                                });
+                            }
+                        }
                         className='w-75' ref={ref => player = ref} controls
                         src="http://imge-ssd-02-l03.imovies.cc/video/imovie_hash_code/13/2019072517020395_high_geo.mp4?md5=qr1g1Rlszl1zKPZhODYAig&expires=1564211833&data=YTozOntzOjc6InVzZXJfaXAiO3M6MTI6IjMxLjE0Ni4yLjE3MCI7czoxMDoidXNlcl9hZ2VudCI7czoxMTU6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS83NS4wLjM3NzAuMTQyIFNhZmFyaS81MzcuMzYiO3M6NzoicmVmZXJlciI7czo1OToiaHR0cHM6Ly93d3cuaW1vdmllcy5jYy9rYS9tb3ZpZXMvNDUwMTQ3MzAzL0Fzc2Fzc2luJ3MtQ3JlZWQiO30="/>
                 </div>
